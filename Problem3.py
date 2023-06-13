fdf1746c75859a322b7865585cfb9212dca4f9a5
Michael Vo <vo.mikey@gmail.com>
2023-06-13 14:39:07 -0700

    Deleted extra lines

--- Problem3.py
+++ Problem3.py
@@ -3,10 +3,12 @@
 # Current date and time :
 # 2014-07-05 14:34:14
 
+#Needed to get datetime
 from datetime import datetime
 
+#created a today varible to hold method
 today = datetime.today()
 
+#printing out datetime in a certain order using % sign
 dt_string = today.strftime("%m/%d/%Y %H:%M:%S")
 print(dt_string)
-
