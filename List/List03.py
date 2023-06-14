c81709596068625256979c260c9f64431718f30d
Michael Vo <vo.mikey@gmail.com>
2023-06-14 14:14:04 -0700

    Created a function and used max function to find the largest number in the list

---
+++ List/List03.py
@@ -0,0 +1,11 @@
+# 3. Write a Python program to get the largest number from a list.
+
+#Function to find largest number using max
+def largest(n):
+    return max(n)
+
+#Varible to hold list of numbers
+num_list = [1,2,3,4,5]
+#Printing using function and variable
+print(largest(num_list))
+
