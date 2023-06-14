752c5371502c28f154eafa2ec1364cfbac774bca
Michael Vo <vo.mikey@gmail.com>
2023-06-14 14:10:02 -0700

    deleted old files

--- Exercises/Problem9.py
+++
@@ -1,9 +0,0 @@
-# 9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
-# exam_st_date = (11, 12, 2014)
-# Sample Output : The examination will start from : 11 / 12 / 2014
-
-#The given list
-exam_st_date = (11, 12, 2014)
-
-#Printing out the examination date using the list provided
-print("The examination will start from: %d/%d/%d"%exam_st_date)
