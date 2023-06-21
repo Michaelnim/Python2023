eefa0ac29a4ffb5f286ccd4679504ddd885c46e5
Michael Vo <vo.mikey@gmail.com>
2023-06-21 14:35:47 -0700

    Module used to randomize elements in list

---
+++ List/List15.py
@@ -0,0 +1,8 @@
+# 15. Write a Python program to shuffle and print a specified list.
+
+# Used a module from random
+from random import shuffle
+
+num_list = [1,2,3,4,5]
+shuffle(num_list)
+print(num_list)
