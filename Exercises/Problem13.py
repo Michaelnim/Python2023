fc5e3847a7af8d76c70a0a6788cb564cae98ebfc
Michael Vo <vo.mikey@gmail.com>
2023-06-21 12:39:40 -0700

    Mult-string used to print out string no need for \n \t

---
+++ Exercises/Problem13.py
@@ -0,0 +1,13 @@
+# 13. Write a Python program to print the following 'here document'.
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+# Printing out string instead of using \n \t used """(3x)
+# It is called a mult-line string
+print("""a string that you "don't" have to escape
+This
+is a ....... multi-line
+here doc string -------> example""")
