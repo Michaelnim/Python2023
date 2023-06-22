ab5d5dd7e0264c98dcc0b5156783078631abf936
Michael Vo <vo.mikey@gmail.com>
2023-06-22 14:27:14 -0700

    Using list(set) to subtract difference then appending the difference to print new list

--- List/List19.py
+++ List/List19.py
@@ -1,8 +1,12 @@
 # 19. Write a Python program to calculate the difference between the two lists.
 
-# Comparing two lists using sum() function
+# Comparing two lists
 def compare(l1, l2):
-    return abs(sum(l1) - sum(l2))
+    # Grabbing the difference from list1 and list2
+    # Adding the difference together
+    new_list = list(set(l1) - set(l2)) + list(set(l2) - set(l1))
+    # Returning it as a new list
+    return new_list
 
 # Testing variables
 list1 = [1,2,3]
