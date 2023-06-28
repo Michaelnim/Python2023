148dd0c3b5e4a2f9d4ac53e5b422ae2581bfee4c
Michael Vo <vo.mikey@gmail.com>
2023-06-28 14:09:47 -0700

    Printing out str x amount of times depending on list of numbers

---
+++ Exercises/Problem26.py
@@ -0,0 +1,10 @@
+# 26. Write a Python program to create a histogram from a given list of integers.
+
+
+
+def histogram(str,l1):
+    for i in l1:
+        print(i * str)
+    
+        
+(histogram("@",[1,2,3,4]))
