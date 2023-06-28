0c545ced31245963568ea04f6c8e74b2e896e655
Michael Vo <vo.mikey@gmail.com>
2023-06-27 17:09:33 -0700

    Random module to randomize an item from the list

---
+++ List/List25.py
@@ -0,0 +1,9 @@
+# 25. Write a Python program to select an item randomly from a list.
+
+# Random module
+import random
+
+# Testing list 
+list1 = ["red","blue","green",1,2,3]
+
+print(random.choice(list1))
