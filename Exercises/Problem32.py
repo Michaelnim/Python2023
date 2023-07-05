f4e825b2d708dbebdd42c9cbe3a8f45a4085af10
Michael Vo <vo.mikey@gmail.com>
2023-07-05 13:47:03 -0700

    Import math to find LCM (needs work)

---
+++ Exercises/Problem32.py
@@ -0,0 +1,9 @@
+# 32. Write a Python program to find the least common multiple (LCM) of two positive integers.
+
+
+import math
+# Used import math to find LCM
+def lcm(num1,num2):
+    return math.lcm(num1,num2)
+
+print(lcm(4,6))
